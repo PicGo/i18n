8eb807888d07e8bcbb509f319213ba1f740b652c
Spades-S <allengefeng@163.com>
2020-04-30 22:41:01 +0800

    :white_check_mark: Test: add objectAdater test cases

--- test/index.js
+++ test/index.js
@@ -1,20 +1,61 @@
 const assert = require('assert');
 const path = require('path');
-const I18n = require('../dist/index').I18n;
+const { I18n, FileSyncAdapter, ObjectAdapter } = require('../dist/index');
 
-const i18n = new I18n({
-  autoReoload: true,
-  language: 'zh',
+const fileSyncAdapter = new FileSyncAdapter({
   localesBaseDir: path.resolve(__dirname, './locales'),
 });
 
-describe('i18n', function(){
+const objectAdapter = new ObjectAdapter({
+  zh: {
+    user: {
+      name: 'PicGo',
+      country: 'China',
+    },
+    report: {
+      singular: ' ${cnt}个报告',
+      plural: '${cnt}个报告',
+    },
+  },
+  en: {
+    user: {
+      name: 'PicGo',
+      country: 'China',
+    },
+    report: {
+      singular: 'only ${cnt} report',
+      plural: '${cnt} reports',
+    },
+  },
+});
 
-  it('translate', () => {
-    assert.equal(i18n.translate('report.plural', { cnt: 2 }), '2个报告');
+describe('i18n', () => {
+  describe('fileSyncAdapter', () => {
+    const i18n = new I18n({
+      adapter: fileSyncAdapter,
+      defaultLanguage: 'zh',
+    });
+
+    it('translate', () => {
+      assert.equal(i18n.translate('report.plural', { cnt: 2 }), '2个报告');
+    });
+    it('setLanguate', () => {
+      i18n.setLanguage('en');
+      assert.equal(i18n.translate('report.plural', { cnt: 2 }), '2 reports');
+    });
   });
-  it('setLanguate', () => {
-    i18n.setLanguage('en');
-    assert.equal(i18n.translate('report.plural', { cnt: 2 }), '2 reports');
+
+  describe('objectAdapter', () => {
+    const i18n = new I18n({
+      adapter: objectAdapter,
+      defaultLanguage: 'zh',
+    });
+    it('translate', () => {
+      assert.equal(i18n.translate('report.plural', { cnt: 2 }), '2个报告');
+    });
+    it('setLanguate', () => {
+      i18n.setLanguage('en');
+      assert.equal(i18n.translate('report.plural', { cnt: 2 }), '2 reports');
+    });
   });
 });
